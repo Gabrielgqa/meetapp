43359fa8b80cd70ca546b71fda2649e1cbdd575d
gabrielgqa <gabrielgqa@gmail.com>
2019-10-05 11:59:14 -0300

    configura sequelize e migration de usuários

---
+++ src/config/database.js
@@ -0,0 +1,12 @@
+module.exports = {
+  dialect: 'postgres',
+  host: 'localhost',
+  username: 'root',
+  password: 'gg96791qa',
+  database: 'db_meetapp',
+  define: {
+    timestamp: true,
+    underscored: true,
+    underscoredAll: true,
+  },
+};
